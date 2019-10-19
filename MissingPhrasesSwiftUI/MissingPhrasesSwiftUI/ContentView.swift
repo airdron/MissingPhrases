3da3c6b84c898c48a074ae7a173f87290afc8992
Andrew Oparin <andreoparin@yandex.ru>
2019-10-20 01:01:43 +0300

    add swift ui project in workspace

---
+++ MissingPhrasesSwiftUI/MissingPhrasesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MissingPhrasesSwiftUI
+//
+//  Created by Andrew Oparin on 20.10.2019.
+//  Copyright © 2019 Andrew Oparin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
